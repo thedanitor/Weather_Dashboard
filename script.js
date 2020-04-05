838ea997b44d716572b0f7a724f9f8d63c2bfe19
Dan Weikart <danmweikart@gmail.com>
2020-04-05 15:10:39 -0700

    Created ajax call.

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //URL: api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 //URL 5 day : api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
+
+
+var citySearch = "Seattle";
+var queryURL1 = "api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=2f51e5636ace798720642f212b20ff1e";
+
+var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f51e5636ace798720642f212b20ff1e"
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+
+
+})
