001a1fcab2e80cab28f5fad0efaf2180c15900b6
Dan Weikart <danmweikart@gmail.com>
2020-04-06 08:56:19 -0700

    added https to api address. AJAX call works now.

--- script.js
+++ script.js
@@ -2,10 +2,15 @@
 //URL 5 day : api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}
 
 
+    
+
+
 var citySearch = "Seattle";
 var queryURL1 = "api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=2f51e5636ace798720642f212b20ff1e";
 
-var queryURL = "api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f51e5636ace798720642f212b20ff1e"
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=2f51e5636ace798720642f212b20ff1e"
+
+$(".fa-search").on("click", function() {
 
 $.ajax({
     url: queryURL,
@@ -14,4 +19,5 @@ $.ajax({
     console.log(response);
 
 
+})
 })
